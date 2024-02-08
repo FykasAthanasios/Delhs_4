169b30e2d722a1609d0a95d046985ecbadf60ae2
Fotis-Nikolid <fotisnikolidais@gmail.com>
2024-02-08 15:40:27 +0200

    changes

--- filesyshandling.c
+++ filesyshandling.c
@@ -37,19 +37,22 @@ bool compare_file(char* path1, char* path2)
       return false;
    }
 
-   char real_path1[strlen(path1) + strlen(parent1_dir) + 1], real_path2;
+   char real_path1[strlen(path1) + strlen(parent1_dir) + 1], real_path2[strlen(path2) + strlen(parent2_dir) + 1];
 
-
+   strcpy(real_path1, parent1_dir);
+   strcat(real_path1, path1);
+   strcpy(real_path2, parent2_dir);
+   strcat(real_path2, path2);
 
    struct stat stat1, stat2;
 
-   if(stat(path1, &stat1) == -1)
+   if(stat(real_path1, &stat1) == -1)
    {
       perror("stat error");
       exit(EXIT_FAILURE);
    }
 
-   if(stat(path2, &stat2) == -1)
+   if(stat(real_path2, &stat2) == -1)
    {
       perror("stat error");
       exit(EXIT_FAILURE);
@@ -60,7 +63,7 @@ bool compare_file(char* path1, char* path2)
       return false;
    }
 
-   int fid1 = open(path1, O_RDONLY | O_NONBLOCK);
+   int fid1 = open(real_path1, O_RDONLY | O_NONBLOCK);
 
    if(fid1 == -1)
    {
@@ -68,7 +71,7 @@ bool compare_file(char* path1, char* path2)
       exit(EXIT_FAILURE);
    }
 
-   int fid2 = open(path2, O_RDONLY | O_NONBLOCK);
+   int fid2 = open(real_path2, O_RDONLY | O_NONBLOCK);
 
    if(fid2 == -1)
    {
